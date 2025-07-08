4e38997e27daade2ebf775257a0f4be78aa3c5ad
Abishek khadka <abishek1234khadka@gmail.com>
2025-07-08 23:52:00 +0545

    New : Added the interface for the Userservice

--- Services/IUserService.cs
+++ Services/IUserService.cs
@@ -1,6 +1,26 @@
-﻿namespace Chat_Application.Services
+﻿using Chat_Application.Models;
+
+namespace Chat_Application.Services
 {
     public interface IUserService
     {
+        Task<IEnumerable<User>> FindUsersAsync();
+
+        Task<User> FindUserByIdAsync(Guid userId);
+        Task<User> FindUserByEmailAsync(string email);
+        Task AddUserAsync(User user);
+        Task DeleteUserAsync(Guid id);
+        Task UpdateUserAsync(User user);
+        Task<bool> UserExistsAsync(Guid userId);
+
+        Task<bool> UserExistsByEmailAsync(string email);
+
+        Task<bool> UserExistsByUsernameAsync(string username);
+
+        Task<IEnumerable<User>> FindUsersAsyncPagination(int page);
+
+        Task<IEnumerable<User>>
+        FindUserByUsernameAsync(string username);
     }
 }
+        
