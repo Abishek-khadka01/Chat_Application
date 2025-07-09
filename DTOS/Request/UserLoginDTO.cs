9a44fe4f8420e452588111b33fb71e0d5efc12eb
Abishek khadka <abishek1234khadka@gmail.com>
2025-07-09 11:52:41 +0545

    New : Added the DTO for login

---
+++ DTOS/Request/UserLoginDTO.cs
@@ -0,0 +1,17 @@
+﻿namespace Chat_Application.DTOS.Request
+{
+    public class UserLoginDTO
+    {
+
+
+        [StringLength(100)]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        [Required(ErrorMessage = "Email is required")]
+        [RegularExpression(@"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$", ErrorMessage = "Invalid email format")]
+        public string Email { get; set; } = null!;
+
+        [Required(ErrorMessage = "Password is required")]
+        [MinLength(8, ErrorMessage = "The password should be of at least 8 characters")]
+        public string Password { get; set; } = null!;
+    }
+}
