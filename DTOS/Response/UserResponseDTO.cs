581f59a49524c1083de4a2d9b5d8832daa0c2703
Abishek khadka <abishek1234khadka@gmail.com>
2025-07-09 12:17:33 +0545

    Update : Added the dto for the user

--- DTOS/Response/UserResponseDTO.cs
+++ DTOS/Response/UserResponseDTO.cs
@@ -1,6 +1,26 @@
-﻿namespace Chat_Application.DTOS.Response
+﻿using Chat_Application.Models;
+
+namespace Chat_Application.DTOS.Response
 {
     public class UserResponseDTO
     {
+
+        public Guid id { get; set; }    
+        public string Username { get; set; } = null!;
+        public string Email { get; set; } = null!;
+
+        public string ProfilePicture { get; set; } = null!;
+
+        public UserResponseDTO (User user)
+        {
+            id = user.Id;
+            Username = user.Username;
+            Email = user.Email;
+            ProfilePicture = user.ProfilePictureUrl ?? string.Empty; // Handle null case for profile picture
+        }
+
+
     }
+
+
 }
