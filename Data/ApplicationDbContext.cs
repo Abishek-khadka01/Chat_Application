ad88cc74e653af734e7633627b71f245c0ec56d9
Abishek khadka <abishek1234khadka@gmail.com>
2025-07-09 12:09:24 +0545

    Updated : User profile Added

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -26,7 +26,7 @@ public partial class ApplicationDbContext : DbContext
 
     public virtual DbSet<User> Users { get; set; }
 
-    
+  
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
