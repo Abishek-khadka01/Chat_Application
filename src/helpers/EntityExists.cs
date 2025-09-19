eea378e88581c119f90b2f48dcbcb1036d6ceb64
Abishek khadka <abishek1234khadka@gmail.com>
2025-09-20 00:26:40 +0545

    Added the filter to check if entity exists

--- src/helpers/EntityExists.cs
+++ src/helpers/EntityExists.cs
@@ -1,31 +1,32 @@
-// using Microsoft.AspNetCore.Http;
-// using Microsoft.AspNetCore.Http.HttpResults;
-// using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
+using chat_application.Data;
 
-// namespace Chat_Application.src.Helpers
-// {
-//     public class EntityExistsFilter<TSchema> : IEndpointFilter where TSchema : class
-//     {
-//         private readonly DbContext _db;
+namespace Chat_Application.src.Helpers
+{
+    public class EntityExistsFilter<TSchema> : IEndpointFilter where TSchema : class
+    {
+        private readonly AppDbContext _db;
 
-//         public EntityExistsFilter(DbContext db)
-//         {
-//             _db = db;
-//         }
+        public EntityExistsFilter(AppDbContext db)
+        {
+            _db = db;
+        }
 
-//         public async ValueTask<object?> InvokeAsync(EndpointFilterInvocationContext context, EndpointFilterDelegate next)
-//         {
-//             // Assume "id" is the first argument in the endpoint method
-//             var id = context.GetArgument<string>(0);
+        public async ValueTask<object?> InvokeAsync(EndpointFilterInvocationContext context, EndpointFilterDelegate next)
+        {
+      
+            var id = context.GetArgument<string>(0);
 
-//             var item = await _db.Set<TSchema>().FindAsync(id);
+            var item = await _db.Set<TSchema>().FindAsync(id);
 
-//             if (item == null)
-//             {
-//                 return TypedResults.NotFound();
-//             }
+            if (item == null)
+            {
+                return TypedResults.NotFound();
+            }
 
-//             return await next(context);
-//         }
-//     }
-// }
+            return await next(context);
+        }
+    }
+}
