9ac5b13b5af63975c5c92ee3ac29d07d23cf5215
Abishek khadka <abishek1234khadka@gmail.com>
2025-09-20 00:26:03 +0545

    added the global using.cs

---
+++ src/using.cs
@@ -0,0 +1,6 @@
+global using Serilog;
+global using FluentValidation;
+global using Microsoft.EntityFrameworkCore;
+global using Chat_Application.src.common;
+global using chat_application.Data;
+global using Microsoft.AspNetCore.Http.HttpResults;
